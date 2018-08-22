535633e4196ee5b23e574e9525759a14d02314e1
Nicole <nicole.sattler@live.com>
2018-08-22 12:38:30 -0500

    Add state and click handler, import book details

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 import { graphql } from 'react-apollo'
 import { getBooks } from '../queries'
+import BookDetails from './BookDetails';
 
 class BookList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selected: null
+    }
+  }
+
+  handleClick = book => {
+    this.setState({
+      selected: book.id
+    })
+  }
 
   displayBooks() {
     let data = this.props.data
@@ -10,7 +23,7 @@ class BookList extends Component {
     if(data.loading) return(<div>Loading books...</div>)
     
     return data.books.map(book => (
-      <li key={ book.id }>{ book.title }</li>
+      <li key={ book.id } onClick={()=> this.handleClick(book) }>{ book.title }</li>
     ))
   }
 
@@ -20,6 +33,9 @@ class BookList extends Component {
         <ul id="book-list">
           { this.displayBooks() }
         </ul>
+        <BookDetails 
+          bookId={this.state.selected}
+        />
       </div>
     )
   }
